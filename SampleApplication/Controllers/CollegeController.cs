da9268d306ce1e25311b32913fcd782ca0370858
Sachi Mahanta <Sachi.Mahanta@civica2.com>
2021-03-22 14:52:34 +0530

    MVC

---
+++ SampleApplication/Controllers/CollegeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SampleApplication.Model.College;
+using SampleApplication.Models;
+
+namespace SampleApplication.Controllers
+{
+    public class CollegeController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: College
+        public ActionResult Index()
+        {
+            return View(db.Colleges.ToList());
+        }
+        public string PrintMessage()
+        {
+            return "<h1>Welcome</h1><p>This is my First Custom Page of my Application.</p>";
+        }
+
+        // GET: College/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            College college = db.Colleges.Find(id);
+            if (college == null)
+            {
+                return HttpNotFound();
+            }
+            return View(college);
+        }
+
+        // GET: College/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: College/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,StudentName,Department")] College college)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Colleges.Add(college);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(college);
+        }
+
+        // GET: College/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            College college = db.Colleges.Find(id);
+            if (college == null)
+            {
+                return HttpNotFound();
+            }
+            return View(college);
+        }
+
+        // POST: College/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,StudentName,Department")] College college)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(college).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(college);
+        }
+
+        // GET: College/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            College college = db.Colleges.Find(id);
+            if (college == null)
+            {
+                return HttpNotFound();
+            }
+            return View(college);
+        }
+
+        // POST: College/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            College college = db.Colleges.Find(id);
+            db.Colleges.Remove(college);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
